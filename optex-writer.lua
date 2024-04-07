911de2af74c8e0d3decc803e319f407f8c1d32d1
Jan Martínek <martij46@fit.cvut.cz>
2024-04-07 19:02:56 +0200

    Breaks writer

--- optex-writer.lua
+++ optex-writer.lua
@@ -5,3 +5,10 @@ Writer.Inline.Str = function(str)
   return str.text
 end
 
+
+Writer.Inline.SoftBreak = function(_, opts)
+  return pandoc.layout.space
+end
+
+Writer.Inline.LineBreak = pandoc.layout.space
+
